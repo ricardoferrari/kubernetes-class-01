9c1cf60649dba453022515edc520f3463a65ee7a
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2022-05-01 21:20:42 -0300

    fix: deployment group

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 app.get('/', (_req, res) => {
   const networkInterfaces = os.networkInterfaces();
 
+  // Para encontrar o serviço o ideal é utilizar a sintaxe do kubernet {SVCNAME}_SERVICE_HOST and {SVCNAME}_SERVICE_PORT
   res.send(`
+    <h2>${process.env.BACKEND_SERVICE_HOST}</h2>
     <h1>Hello from this NodeJS app! ${JSON.stringify(networkInterfaces)}</h1>
     <p>Try sending a request to /error and see what happens</p>
   `);
